91573a4048020d61f83b302266b25fe85c4dfce5
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-08-04 14:09:11 +0900

    add Chisu playlist functions - add Websocket - add API - version up to 1.2.0

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.11.0")
+
     testImplementation(kotlin("test"))
 }
 
