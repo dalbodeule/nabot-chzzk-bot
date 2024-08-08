9879fafa25d03d6e010c07ad3ca57bf2545b79f7
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-08-08 17:06:20 +0900

    add Session flows (2x) - fix CORS

--- webserver/src/main/kotlin/space/mori/chzzk_bot/webserver/Main.kt
+++ webserver/src/main/kotlin/space/mori/chzzk_bot/webserver/Main.kt
@@ -52,7 +52,7 @@ val server = embeddedServer(Netty, port = 8080, ) {
         allowMethod(HttpMethod.Patch)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Get)
-        allowOrigins { it == dotenv["FRONTEND"] }
+        allowHost(dotenv["FRONTEND"] ?: "localhost:3000")
     }
     install(Sessions) {
         cookie<UserSession>("user_session", storage = SessionStorageMemory()) {}
