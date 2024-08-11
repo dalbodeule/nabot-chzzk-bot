e68794ce3351cc0e6d00cabe81845211caef6d91
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-08-11 16:39:35 +0900

    debug discord login (8x)

--- webserver/src/main/kotlin/space/mori/chzzk_bot/webserver/ktorClient.kt
+++ webserver/src/main/kotlin/space/mori/chzzk_bot/webserver/ktorClient.kt
@@ -2,9 +2,12 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
 val applicationHttpClient = HttpClient(CIO) {
     install(ContentNegotiation) {
-        json()
+        json(json = Json {
+            ignoreUnknownKeys = true
+        })
     }
 }
